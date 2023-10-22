15068d13b0437e8549f23c40030e48c78b09854a
Andrew Yalunin <148551592+andyalu@users.noreply.github.com>
2023-10-22 22:24:16 +0300

    Add files via upload

---
+++ src/components/ContactSet.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="contact-set">
+    <a
+      :href="contact.link"
+      class="contact-item"
+      v-for="contact in contacts"
+      :key="contact.name"
+      target="_blank"
+    >
+      <img
+        :src="require('../assets/img/icons/' + contact.img)"
+        width="20"
+        height="20"
+        :alt="contact.name"
+        loading="lazy"
+      />
+      {{ contact.text }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactSet",
+  data() {
+    return {
+      contacts: [
+        {
+          name: "location",
+          text: "Long Melford, Sudbury, UK",
+          img: "location.png",
+          link: "https://www.google.com/maps/place/Long+Melford,+Sudbury/@52.0793216,0.7013253,14z/data=!3m1!4b1!4m6!3m5!1s0x47d8545f1342987b:0x5c8bb4fd479aa9a3!8m2!3d52.074356!4d0.71633!16zL20vMDg3Z2Rf?entry=ttu",
+        },
+        {
+          name: "telegram",
+          text: "@andyalu",
+          img: "telegram.png",
+          link: "https://telegram.me/andyalu",
+        },
+        {
+          name: "email",
+          text: "yalunin077@gmail.com",
+          img: "email.png",
+          link: "mailto:yalunin077@gmail.com",
+        },
+        {
+          name: "phone",
+          text: "+447798501078",
+          img: "phone.png",
+          link: "tel:+447798501078",
+        },
+        {
+          name: "github",
+          text: "github.com/andyalu",
+          img: "github.png",
+          link: "https://github.com/andyalu",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.contact-set {
+  width: 100%;
+}
+.contact-item {
+  display: flex;
+  align-items: center;
+  padding: 1.6rem;
+  border: 1px solid hsla(0, 0%, 100%, 0.2);
+  background-color: var(--main-bg-color);
+  backdrop-filter: blur(5px);
+  border-radius: var(--box-border-radius);
+  border: var(--box-border);
+  box-shadow: var(--box-shadow);
+  margin-bottom: 0.3rem;
+  transition: all 0.2s ease;
+}
+.contact-item:hover {
+  background-color: var(--contact-item-hover-bg);
+  border: var(--box-border);
+}
+.contact-item img {
+  margin-right: 1.5rem;
+  height: auto;
+  filter: drop-shadow(var(--regular-text-shadow));
+}
+@media (max-width: 600px) {
+  .contact-item {
+    padding: 1rem;
+    font-size: 0.9rem;
+  }
+}
+@media (max-width: 350px) {
+  .contact-item {
+    padding: 1.5rem 0.8rem;
+    font-size: 0.9rem;
+  }
+  .contact-item img {
+    margin-right: 0.5rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="home-section" id="home" v-motion-fade>
+    <div class="container">
+      <h2 class="section-heading">_ Hello</h2>
+      <IntroAnimation />
+      <p class="section-text">
+        My name is Andrew, I'm a Front End Developer based in Long Melford,
+        Sudbury, UK.
+        <br />
+        I am working on web interfaces that should be good looking, simple and
+        enjoyable to use.<br />
+        If you are interested in cooperation and transforming your ideas into a
+        great projects, please contact me in any way convenient for you.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import IntroAnimation from "@/components/IntroAnimation.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    IntroAnimation,
+  },
+};
+</script>
+
+<style>
+.home-section .section-heading {
+  color: var(--home-color);
+}
+</style>
